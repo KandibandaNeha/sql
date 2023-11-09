f014a41bc5857550677f8195336438e910da7aa8
kandibanda.Neha <nehakandibanda05@gmail.com>
2023-11-09 11:27:04 +0530

    updated with data

---
+++ bankdata.sql
@@ -0,0 +1,12 @@
+create database emptable;
+use emptable;
+create table Emp
+(
+EmployeeID int,
+FirstName varchar(255),
+LastName  varchar(255),
+Emial varchar(255),
+AddressLine varchar(255),
+City varchar(255)
+);
+show databases;
